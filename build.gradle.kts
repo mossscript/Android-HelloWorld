8f9f7ea7eb1507dfd9e9838418dcdc60eb1386b3
mossscript <mahdi.dev.404@gmail.com>
2025-07-27 02:39:35 +0330

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,9 @@
+// Top-level build file
 plugins {
-    id("com.android.application") version "8.1.0"
-    kotlin("android") version "1.9.0"
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
 
-android {
-    namespace = "io.mossscript.hello"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "io.mossscript.hello"
-        minSdk = 21
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
-        }
-    }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
