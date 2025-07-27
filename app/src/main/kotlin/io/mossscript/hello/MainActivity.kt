ba35bdad303c6183d23eda9a5bfbf3107a6d34a0
mossscript <mahdi.dev.404@gmail.com>
2025-07-27 19:35:34 +0330

    update

--- app/src/main/kotlin/io/mossscript/hello/MainActivity.kt
+++ app/src/main/kotlin/io/mossscript/hello/MainActivity.kt
@@ -1,30 +1,11 @@
 package io.mossscript.hello
 
 import android.app.Activity
-import android.graphics.Color
 import android.os.Bundle
-import android.view.Gravity
-import android.widget.TextView
-import android.widget.LinearLayout
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val layout = LinearLayout(this).apply {
-            orientation = LinearLayout.VERTICAL
-            gravity = Gravity.CENTER
-            setBackgroundColor(Color.parseColor("#DDDDDD"))
-        }
-
-        val textView = TextView(this).apply {
-            text = getString(R.string.message_1)
-            textSize = 20f
-            setTextColor(Color.parseColor("#222222")) 
-            gravity = Gravity.CENTER
-        }
-
-        layout.addView(textView)
-        setContentView(layout)
+        setContentView(R.layout.activity_main)
     }
 }
